ac049572082da330345b67640876feeb33e29296
tomas agnese <tagnese1985@gmail.com>
2022-02-27 17:46:40 -0300

    Add navbar logo and style

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :user_stocks
   devise_for :users
-  root 'welcome#index'
+  root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
 
