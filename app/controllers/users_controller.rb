12b62d1d4c7b1a0a25dd28f00b47eeddafa56362
tomas agnese <tagnese1985@gmail.com>
2022-03-07 01:04:38 -0300

    User route and view created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'users', to: 'users#index'
 
   resources :stocks, only: [:create]
 end
